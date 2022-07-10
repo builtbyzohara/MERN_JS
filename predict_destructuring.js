4660aa0e98a1713bf51b2b1dcb588bd183d86bec
builtbyzohara <98929607+builtbyzohara@users.noreply.github.com>
2022-07-10 13:18:19 -0500

    Add files via upload

---
+++ predict_destructuring.js
@@ -0,0 +1,79 @@
+Problem 1:
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+// Tesla
+console.log(otherRandomCar)
+// Mercedes
+
+
+Problem 2:
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+// will give an error
+console.log(otherName);
+// Elon - this is my best guess
+
+//The answer is reference error: name is not defined
+
+
+
+///Problem 4:
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+false
+console.log(first == third);
+true
+
+
+Problem 3:
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+//12345
+console.log(hashedPassword);
+// error, since there is no hashedPassword for the person yet - yes! got this right - this will return an undefined
+
+
+Problem 5:
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+//value
+console.log(secondKey);
+[1,5,1,8,3,3]
+console.log(secondKey[0]);
+1
+console.log(willThisWork);
+5
+
+
